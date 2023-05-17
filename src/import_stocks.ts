ac39c7ef506782dc7d54616b0b99a20c2b0c493d
Mohammed Alshami <alshami@juleb.com>
2023-05-17 10:44:43 +0300

    test url fix

--- src/import_stocks.ts
+++ src/import_stocks.ts
@@ -6,7 +6,7 @@ import { existsSync, promises } from "fs";
 class Script {
   paramsFilePath: string = "./params.json";
   // julebApiUrl = "https://api.juleb.com/agent_receiver/retailpro";
-  julebApiUrl = "https://261d-176-18-80-157.ngrok-free.app";
+  julebApiUrl = "https://261d-176-18-80-157.ngrok-free.app/retailpro";
 
   constructor() {}
   async fetchStoreName(connection, store_code, identifier) {
