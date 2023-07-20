2645a524400a2ce17b1bc9690c00b32bce682b3c
Mohammed Alshami <alshami@juleb.com>
2023-07-20 11:39:19 +0300

    back

--- src/sync.ts
+++ src/sync.ts
@@ -6,8 +6,8 @@ import { existsSync, promises } from "fs";
 class Script {
   paramsFilePath: string = "./params.json";
   bookmarkFilePath: string = "./bookmarks.json";
-  // julebApiUrl = "https://api.juleb.com/agent_receiver/retailpro";
-  julebApiUrl = "https://fca7-5-110-47-30.ngrok-free.app/retailpro";
+  julebApiUrl = "https://api.juleb.com/agent_receiver/retailpro";
+  // julebApiUrl = "https://fca7-5-110-47-30.ngrok-free.app/retailpro";
 
   constructor() {}
 
